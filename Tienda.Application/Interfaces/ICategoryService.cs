f12b0008f45e0584c827544d15db027b4e54f7c2
jesus <jesus@JESUS>
2026-05-28 21:08:22 -0400

    CategoryImplementation

---
+++ Tienda.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tienda.Application.Dtos;
+
+namespace Tienda.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDTO>> GetAllCategoriesAsync();
+        Task<CategoryDTO> UpdateCategoryAsync(string currentName,string newName);
+        Task<CategoryDTO> DelateCategoryAsync(string nombre);
+        Task<CategoryDTO> AddCategoryAsync(CategoryDTO dto);
+    }
+}
